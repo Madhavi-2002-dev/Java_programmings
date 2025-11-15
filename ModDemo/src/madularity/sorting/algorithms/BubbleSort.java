04923b3774d298fe2eaf0d8b9d88000e09d90ef2
Madhavi <arepatimadhavi@gmail.com>
2025-11-15 10:02:18 +0530

    Add files via upload

---
+++ ModDemo/src/madularity/sorting/algorithms/BubbleSort.java
@@ -0,0 +1,13 @@
+package madularity.sorting.algorithms;
+
+import java.util.List;
+
+public class BubbleSort {
+
+	public List<String> sort(List<String> names) {
+		// TODO Auto-generated method stub
+		return names;
+	}
+	
+
+}
